c9958ce587bf846fd0d1847127c87f287d049cd7
Gonçalo <goncaloscravo@gmail.com>
2024-03-19 09:20:45 +0000

    Add: navbar

---
+++ frontend/frontend-project/src/components/CreateUserRequest.jsx
@@ -0,0 +1,17 @@
+export async function CreateUserRequest(user) {
+  const response = await fetch("http://localhost:8080/api/user", {
+    method: "PUT",
+    body: JSON.stringify({ user }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const respData = response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to create a user");
+  }
+
+  return respData.message;
+}
