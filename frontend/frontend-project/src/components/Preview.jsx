bcc7d68522b6c990c56fd7a4654763e129a01343
Gonçalo <goncaloscravo@gmail.com>
2024-03-27 13:03:30 +0000

    Add: preview with tasks and task component

--- frontend/frontend-project/src/components/Preview.jsx
+++ frontend/frontend-project/src/components/Preview.jsx
@@ -1,28 +1,60 @@
-import { Paper, Title, Text, Button } from "@mantine/core";
+import { Paper, Title, Text, Button, Anchor } from "@mantine/core";
+import "./Preview.css";
+import Tasks from "./Tasks";
 
-export default function Prewiew({ selectedProject, notPreviewing }) {
+export default function Preview({ selectedProject, notPreviewing }) {
   const project = selectedProject[0];
 
   return (
-    <Paper shadow="md" p="xl" radius="md" className={`card ${project.color}`}>
-      <div>
-        <Button onClick={notPreviewing} />
-        <Text className="category" size="xs">
-          {project.id}
-        </Text>
-        <Title order={3} className="title">
-          {project.name}
+    <div className="previewing">
+      <Paper
+        shadow="xl"
+        p="xl"
+        radius="md"
+        className={`card ${project.color} project`}
+      >
+        <div>
+          <Text c="dimmed" size="sm" ta="center" mt={5}>
+            Want to go back?{" "}
+            <Anchor
+              className="anchor"
+              size="sm"
+              component="button"
+              onClick={notPreviewing}
+            >
+              Cancel
+            </Anchor>
+          </Text>
+          <Text className="category" size="x">
+            {`Id: ${project.id}`}
+          </Text>
+          <Title order={3} className="title">
+            {project.name}
+          </Title>
+          <Text className="category" size="xs">
+            {project.date}
+          </Text>
+          <Text className="category" size="x">
+            {project.description}
+          </Text>
+        </div>
+      </Paper>
+      <Paper
+        shadow="xl"
+        p="xl"
+        radius="md"
+        className={`card-tasks ${project.color}`}
+      >
+        <Title order={3} className="title-tasks">
+          Tasks
         </Title>
-        <Text className="category" size="xs">
-          {project.date}
-        </Text>
-        <Text className="category" size="xs">
-          {project.description}
-        </Text>
-      </div>
-      <Button variant="white" color="dark">
-        Add task
-      </Button>
-    </Paper>
+        <Tasks seletedProject={project} />
+        <div className="bottom-btn">
+          <Button variant="white" color="dark" className="btn-task">
+            Add task
+          </Button>
+        </div>
+      </Paper>
+    </div>
   );
 }
