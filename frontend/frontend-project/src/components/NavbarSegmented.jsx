9b1f3eb8d34c0f504e7ff271512675010b6b2b7a
Gonçalo <goncaloscravo@gmail.com>
2024-03-28 12:54:55 +0000

    Add: nav issue

--- frontend/frontend-project/src/components/NavbarSegmented.jsx
+++ frontend/frontend-project/src/components/NavbarSegmented.jsx
@@ -1,26 +1,8 @@
 import { SegmentedControl, Text } from "@mantine/core";
-import { IconDatabaseImport, IconLogout } from "@tabler/icons-react";
+import { IconLogout } from "@tabler/icons-react";
 import classes from "./NavbarSegmented.module.css";
 
-const tabs = {
-  account: [{ link: "", label: "Projects", icon: IconDatabaseImport }],
-};
-
 export default function NavbarSegmented({ userLogout, userDto }) {
-  const links = tabs.account.map((item) => (
-    <a
-      className={classes.link}
-      href={item.link}
-      key={item.label}
-      onClick={(event) => {
-        event.preventDefault();
-      }}
-    >
-      <item.icon className={classes.linkIcon} stroke={1.5} />
-      <span>{item.label}</span>
-    </a>
-  ));
-
   return (
     <div className={classes.orientation}>
       <nav className={classes.navbar}>
@@ -43,7 +25,7 @@ export default function NavbarSegmented({ userLogout, userDto }) {
           />
         </div>
 
-        <div className={classes.navbarMain}>{links}</div>
+        <div className={classes.navbarMain}></div>
 
         <div className={classes.footer}>
           <a href="#" className={classes.link} onClick={userLogout}>
